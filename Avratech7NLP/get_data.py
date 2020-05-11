296e3e3e575e3830eaa0a87863d2774ef2bc4f52
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-05-11 13:15:13 +0300

    add limit function

--- Avratech7NLP/get_data.py
+++ Avratech7NLP/get_data.py
@@ -45,13 +45,22 @@ def _stemming_the_paragraphs(paragraph):
 	return ' '.join([stemmer.stem(word) for word in paragraph.split(' ')])
 
 
+def _limit_size_of_string(l, min=200):
+	in_list = sorted(l)         
+	while(len(in_list[0]) < min and len(in_list) > 1):
+		in_list[1] += ' ' + in_list[0]
+		in_list.pop(0)
+		in_list = sorted(in_list)
+	return in_list
+
+
 def get_clean_data(url, *label):
 	'''
 	 This is the function that calld from this module
 	 the function accepts url and label and return clean_paragraph object.
 	'''
 	origin_data = _get_wiki_text(url)
-	return clean_paragraph(origin_data, [_stemming_the_paragraphs(_remove_stop_words(_basic_clean(p))) for p in origin_data],label, url)
+	return clean_paragraph(origin_data, _limit_size_of_string([_stemming_the_paragraphs(_remove_stop_words(_basic_clean(p))) for p in origin_data]),label, url)
 	
 
 
